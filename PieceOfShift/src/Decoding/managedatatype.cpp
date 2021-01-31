d6b1c3ddf43390adb720719bf37192fd5590ca32
haakwa <hkon.waage@gmail.com>
2021-01-31 12:12:19 +0100

    Added comments
    Added comments to the managedatatype cpp file

--- PieceOfShift/src/Decoding/managedatatype.cpp
+++ PieceOfShift/src/Decoding/managedatatype.cpp
@@ -17,14 +17,17 @@ void Decoder::checkData(quint16 &id,quint8 &dataSize, QByteArray &data)
 {
     //ulong Data = CanSplitter::newData::&data;
     int N = sizeof(IDTable)/sizeof (IDTable[0]);
-
-    const ushort *ID = qBinaryFind(IDTable, IDTable + N,
-                                  id);
+    //use binary find algorithm to match the id recieved with a named id
+    const ushort *ID = qBinaryFind(IDTable, IDTable + N, id);
     //setup name, and find right datatype
     //also resize the data and set it to qvariant
     quint16 index = ID-IDTable;
+    //use the identified index to find the linking name
     name = ValueNames[index];
+    //cast a datatype based on index
     DataType dataType = DataType(index);
+    //resize data, even though this is done in the cansplitter
+    //its implimented to make the Qvariant as small as possible
     data.resize(dataSize);
 
 
