0735a022348f08045a104c8b49ddbeb5034af8a4
Hanne Kyllo Ødegård <hannekod@stud.ntnu.no>
2021-03-17 17:31:03 +0100

    add start of dataStruct recognition

--- PieceOfShift/src/Processing/datamanager.cpp
+++ PieceOfShift/src/Processing/datamanager.cpp
@@ -2,6 +2,7 @@
 #include "velocityprocessingunit.h"
 #include "accelerationprocessingunit.h"
 #include "accelerationvelocityunit.h"
+#include "datastructs.h"
 
 DataManager::DataManager()
 {
@@ -9,19 +10,19 @@ DataManager::DataManager()
      * maybe refactor to separate function
      */
     VelocityProcessingUnit* vpu = new VelocityProcessingUnit();
-    processingUnits.append(vpu);
+//    processingUnits.append(vpu);
     AccelerationProcessingUnit* apu = new AccelerationProcessingUnit();
-    processingUnits.append(apu);
+//    processingUnits.append(apu);
     AccelerationVelocityUnit* avu = new AccelerationVelocityUnit();
-    processingUnits.append(avu);
+//    processingUnits.append(avu);
 
     // Connect newData signal to corresponding DataManager signal
-    connect(vpu, &VelocityProcessingUnit::newData,
-            this, &DataManager::newVelocity);
-    connect(apu, &AccelerationProcessingUnit::newData,
-            this, &DataManager::newAcceleration);
-    connect(avu, &AccelerationVelocityUnit::newData,
-            this, &DataManager::newAccelerationVelocity);
+//    connect(vpu, &VelocityProcessingUnit::newData,
+//            this, &DataManager::newVelocity);
+//    connect(apu, &AccelerationProcessingUnit::newData,
+//            this, &DataManager::newAcceleration);
+//    connect(avu, &AccelerationVelocityUnit::newData,
+//            this, &DataManager::newAccelerationVelocity);
 
 
     connect(&canServer, &CANServer::dataReceived,
@@ -43,20 +44,32 @@ DataManager::DataManager()
 DataManager::~DataManager()
 {
     // Delete all processing units on destruction to avoid memory leak
-    for (auto processingUnit : processingUnits) {
-        delete processingUnit;
-    }
+//    for (auto processingUnit : processingUnits) {
+//        delete processingUnit;
+//    }
 }
 
 void DataManager::addData(const QString& name, const DataType &dataType, const QVariant &data)
 {
+    /*
+    // WRITE
+    QByteArray ba;
+    QDataStream stream(&ba, QIODevice::ReadWrite);
+    DataStructs::VCUStatus vcuStatus = {true, true, true, true, true, true, true, true, true, 10.0f, 10.0f};
+    stream << vcuStatus;
+    */
+
+    QByteArray vectorData = data.value<QVariantList>().first().value<QPair<uchar, QByteArray>>().second;
+    QDataStream stream(&vectorData, QIODevice::ReadWrite);
+    DataStructs::DataStruct dataStrict = DataStructs::DataStruct::findChild(&name, Qt::FindChildrenRecursively);
+
     // Find processing unit with correct data type and add data
-    ProcessingUnit* processingUnit =
-            *std::find_if(processingUnits.begin(),
-                          processingUnits.end(),
-                          [&dataType](auto x)
-                          { return x->dataType() == dataType; });
-    QtConcurrent::run(processingUnit, &ProcessingUnit::addData, name, data);
+//    ProcessingUnit* processingUnit =
+//            *std::find_if(processingUnits.begin(),
+//                          processingUnits.end(),
+//                          [&dataType](auto x)
+//                          { return x->dataType() == dataType; });
+//    QtConcurrent::run(processingUnit, &ProcessingUnit::addData, name, data);
     //processingUnit->addData(name, data);
 }
 
