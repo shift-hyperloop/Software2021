dec0c1fc9a46190d2a7e43a11934a12efa094d08
Mathias Rønning <ronning2504@gmail.com>
2021-04-07 18:19:12 +0200

    Fix wrong include

--- PieceOfShift/src/main.cpp
+++ PieceOfShift/src/main.cpp
@@ -5,7 +5,7 @@
 #include <qqml.h>
 #include "Decoding/canserver.h"
 #include "Processing/datamanager.h"
-#include "CustomPlotItem.h"
+#include "customplotitem.h"
 #include "qbytearray.h"
 
 int main(int argc, char *argv[])
